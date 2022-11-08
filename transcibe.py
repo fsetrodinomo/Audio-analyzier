89accde73a07e07e7716b25c401ec12328369806
Fauve <fsetrodinomo@protonmail.com>
2022-11-08 16:27:35 +0100

    started on transcribe file, added a parser with arguments

---
+++ transcibe.py
@@ -0,0 +1,10 @@
+import argparse
+import os
+import utils
+from Lib.api_secret import API_KEY
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('audio_file', help='url to file or local audio filename')
+    parser.add_argument('--local', action='store_true' , help='must be set if audio_file is a local filename')
+    parser.add_argument('--api_key', action='store', help='API_KEY')
