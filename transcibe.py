444d7b62949fcc4c902c221a7a647baabdf8429f
Fauve <fsetrodinomo@protonmail.com>
2022-11-10 13:48:51 +0100

    added a parser to check if API_KEY exist

--- transcibe.py
+++ transcibe.py
@@ -7,3 +7,11 @@ def main():
     parser.add_argument('audio_file', help='url to file or local audio filename')
     parser.add_argument('--local', action='store_true' , help='must be set if audio_file is a local filename')
     parser.add_argument('--api_key', action='store', help='API KEY')
+
+    args = parser.parse_args()
+
+    if args.api_key is None :
+        args.api_key = os.getenv("AAI_API_KEY")
+        if args.api_key is None:
+            raise RuntimeError("AAI_API_KEY environment variable not set. Try setting it now, or passing in your "
+                               "API key as a command line argument with `--api_key`.")
